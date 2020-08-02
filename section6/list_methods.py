7ccec7d8d817d73e0a2ab8e20017c313732721dc
ayoung90 <ayoung90@live.com.au>
2020-08-02 18:09:54 +1000

    List operations

---
+++ section6/list_methods.py
@@ -0,0 +1,13 @@
+spam = ['hey','howdy','hiya','hello']
+print(spam)
+print(spam.index('hiya'))
+
+spam.append('hi')
+
+spam.insert(0,'privet')
+print(spam)
+
+spam.remove('hello')
+
+spam.sort()
+print(spam)
--- section6/list_type.py
+++ section6/list_type.py
@@ -1,11 +1,43 @@
-spam = ['cat','bat','rat','elephant']
+spam = ['cat', 'bat', 'rat', 'elephant']
 
 print(spam[2])
 
-spam2 = [['fish','shark'],'bat','rat','elephant']
+spam2 = [['fish', 'shark'], 'bat', 'rat', 'elephant']
 
 print(spam2[0])
 print(spam2[0][1])
 
-spam[2:4] = ['CAT','MOOSE','BEAR']
+spam[2:4] = ['CAT', 'MOOSE', 'BEAR']
 print(spam)
+
+del spam[2]
+
+print(spam)
+
+print('MOOSE' in spam)
+
+# Iterate over lists by item or index
+for item in spam:
+    print(item)
+
+for i in range(0, len(spam)):
+    print(spam[i])
+
+print(len(spam))
+
+cat = ['fat', 'orange', 'loud']
+
+size, color, disposition = cat
+
+print(size)
+print(color)
+print(disposition)
+
+# swap variables
+a = 'AAA'
+b = 'BBB'
+
+a, b = b, a
+
+print(a)
+print(b)
