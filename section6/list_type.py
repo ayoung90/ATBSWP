c8a58e499aede3427659d0594e86810ca0f701c6
ayoung90 <ayoung90@live.com.au>
2020-08-02 17:53:55 +1000

    Add lists file

---
+++ section6/list_type.py
@@ -0,0 +1,11 @@
+spam = ['cat','bat','rat','elephant']
+
+print(spam[2])
+
+spam2 = [['fish','shark'],'bat','rat','elephant']
+
+print(spam2[0])
+print(spam2[0][1])
+
+spam[2:4] = ['CAT','MOOSE','BEAR']
+print(spam)
