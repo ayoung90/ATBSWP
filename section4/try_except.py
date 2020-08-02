ec963dfc9a565d20699842db9b072d531a4f7452
ayoung90 <ayoung90@live.com.au>
2020-08-02 17:22:44 +1000

    Reformat file

--- section4/try_except.py
+++ section4/try_except.py
@@ -1,9 +1,9 @@
-#create an exception
+# create an exception
 def div42by(divideBy):
-  try:
-    return 42 / divideBy
-  except ZeroDivisionError:
-    print('ERROR: Cannot divide by 0')
+    try:
+        return 42 / divideBy
+    except ZeroDivisionError:
+        print('ERROR: Cannot divide by 0')
 
 print(div42by(2))
 print(div42by(12))
@@ -11,14 +11,14 @@ print(div42by(0))  # :)
 print(div42by(2))
 
 def catCounter():
-  print('How many cats do you have? Please enter:')
-  numCats = input()
-  try:
-    if int(numCats) >= 4:
-      print('Thats too many cats!')
-    else:
-      print('That is an acceptable number of cats')
-  except ValueError:
-    print('ERROR: input was not a valid number')
+    print('How many cats do you have? Please enter:')
+    numCats = input()
+    try:
+        if int(numCats) >= 4:
+            print('Thats too many cats!')
+        else:
+            print('That is an acceptable number of cats')
+    except ValueError:
+        print('ERROR: input was not a valid number')
 
 catCounter()
