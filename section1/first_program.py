f1f6bb448e11d1a9043f47a35189d2bb390a2fa1
ayoung90 <ayoung90@live.com.au>
2020-08-02 16:43:20 +1000

    Add section 3 examples

--- section1/first_program.py
+++ section1/first_program.py
@@ -2,5 +2,16 @@
 
 print('Hello world!')
 print('What is your name?')
-myName = input()
+myName = input() #request name
 print('It is good to meet you, ' + myName)
+print('What is your age?') 
+myAge = input() #request age
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+
+print('What would you like to do? (Bye)')
+myInput = input()
+
+if myInput == 'Bye':
+  print('Ok. Bye ' + myName + ' (' + myAge + ')')
+else:
+  print('Sorry I dont understand: ' + myInput)
---
+++ section3/built_in_functions.py
@@ -0,0 +1,13 @@
+import random
+import sys
+import os
+import math
+import pyperclip
+
+print(random.randint(1, 10))
+sys.exit()
+print('Goodbye')
+
+pyperclip.copy('Hello world')
+
+print(pyperclip.paste())
